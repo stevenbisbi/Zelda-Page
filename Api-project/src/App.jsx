405a06155d5d256aac9c52872f7c2b270e156272
Michael <stevenex13@gmail.com>
2025-02-09 12:25:11 -0500

    second commit

--- Api-project/src/App.jsx
+++ Api-project/src/App.jsx
@@ -1,14 +1,14 @@
-import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 import { Home } from "./Pages/Home";
 
 export function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- Api-project/src/Pages/Home.jsx
+++ Api-project/src/Pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { ZeldaApi } from "../Api/object.api";
+("../Api/object.api");
+import { ZeldaList } from "../components/ZeldaList";
 
 export function Home() {
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     async function api() {
-         
+      const res = await ZeldaApi();
+      setData(res.data);
+      console.log(data);
     }
+    api();
   }, []);
 
-  return <div>HOme</div>;
+  return <ZeldaList />;
 }
